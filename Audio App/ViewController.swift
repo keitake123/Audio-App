74e6163cacae37bdc1e8913632dc41f3b88e5fe7
keitake123 <keitaketsuna@gmail.com>
2024-08-07 19:56:16 +0900

    Initial Commit

---
+++ Audio App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Audio App
+//
+//  Created by Keiichi Taketsuna on 2024/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBlue
+    }
+
+
+}
+
