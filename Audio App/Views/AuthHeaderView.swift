2d3ac6b0da2f0946e58f1941404e942bb89b65d4
keitake123 <keitaketsuna@gmail.com>
2024-08-09 16:28:12 +0900

    aa

--- Audio App/Views/AuthHeaderView.swift
+++ Audio App/Views/AuthHeaderView.swift
@@ -9,30 +9,60 @@ import UIKit
 
 class AuthHeaderView: UIView {
     
-        // MARK: - Variables
+    // MARK: - UI Components
+    private let logoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.image = UIImage(named: "logo")
+        return iv
+    }()
+    
+    private let Titlelabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 26, weight: .bold)
+        label.text = "Error"
+        return label
+    }()
+    
+    private let SubTitlelabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.text = "Error"
+        return label
+    }()
         
-        
-        // MARK: - UI Components
-        
-        
-        // MARK: - Lifecycle
-       
-    // MARK: - UI Setup
+    // MARK: - Lifecycle
+    init(title: String, subtitle: String){
+        super.init(frame: .zero)
+        self.Titlelabel.text = title
+        self.SubTitlelabel.text = subtitle
+        self.setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+      // MARK: - UI Setup
     private func setupUI() {
-        self.view.addSubview(<#T##UIView#>)
-        <#T##UIView#>.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(logoImageView)
+        self.addSubview(Titlelabel)
+        self.addSubview(SubTitlelabel)
+        
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        Titlelabel.translatesAutoresizingMaskIntoConstraints = false
+        SubTitlelabel.translatesAutoresizingMaskIntoConstraints = false
         
         
         NSLayoutConstraint.activate([
-            <#T##UIView#>.topAnchor.constraint(equalTo: self.view.topAnchor)
-            <#T##UIView#>.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
-            <#T##UIView#>.leadingAnchor.constraint(equalTo: self.view.leadingAnchor)
-            <#T##UIView#>.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
-            
-            <#T##UIView#>.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
-            <#T##UIView#>.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+            self.logoImageView.topAnchor.constraint(equalTo: self.layoutMarginsGuide.topAnchor, constant: 16),
+            self.logoImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            self.logoImageView.widthAnchor.constraint(equalToConstant: 90),
+            self.logoImageView.heightAnchor.constraint(equalTo: logoImageView.widthAnchor),
         ])
     }
-
-
-
+}
